0269e1514e2d0bc103bc664ffc6e31be427fa561
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-04-18 23:20:04 +0600

    added nav height and shadow

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           {open ? <AiOutlineCloseSquare/> : <AiOutlineBars/>}
         </div>
 
-        <ul className={`h-[10.5rem] md:h-auto md:flex md:items-center md:pb-0 bg-slate-100 rounded-b-[.8rem] md:rounded-b-[0rem] md:bg-white absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+        <ul className={`shadow-xl md:h-auto md:flex md:items-center md:pb-0 bg-slate-100 rounded-b-[.8rem] md:rounded-b-[0rem] md:bg-white absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-[3rem] " : "top-[-700%]"
           }`} >
 
@@ -38,6 +38,10 @@ const Navbar = () => {
           </li>
           <li className="linkBox md:ml-8 text-xl md:my-0 my-7">
           <NavLink to='/about' className={({ isActive }) =>  isActive? "active" : "deActive"} >About</NavLink>
+          </li>
+
+          <li className="linkBox md:ml-8 text-xl md:my-0 my-7">
+          <NavLink to='/login' className={({ isActive }) =>  isActive? "active" : "deActive"} >Login</NavLink>
           </li>
 
 
